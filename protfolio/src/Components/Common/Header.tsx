f1c2f940f8960a6b1e7f60a4274021d5b541163e
Anil <anil@gmail.com>
2022-10-07 17:45:36 +0530

    add file dowload change

--- protfolio/src/Components/Common/Header.tsx
+++ protfolio/src/Components/Common/Header.tsx
@@ -5,14 +5,14 @@ import myimage from "./../../img/Gleb.png";
 const Header = () => {
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch('SamplePDF.pdf').then(response => {
+    fetch('Resume.pdf').then(response => {
         response.blob().then(blob => {
             // Creating new object of PDF file
             const fileURL = window.URL.createObjectURL(blob);
             // Setting various property values
             let alink = document.createElement('a');
             alink.href = fileURL;
-            alink.download = 'SamplePDF.pdf';
+            alink.download = 'Resume.pdf';
             alink.click();
         })
     })
